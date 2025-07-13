1589be7b59626b687228d67fcf825a4456191855
kapcodeO <kapilojha953@gmail.com>
2025-07-13 15:46:42 +0530

    added main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+# Route 1: \
+# root node to check everything is fine
+# returns {"message" : "Hello World"}
+
+# Route 2: /translation
+# store a translation and run in background tasks
+# returns a translation_id
+
+# Route 3: /results
+# takes in a translation_id 
+# returns the final translation
