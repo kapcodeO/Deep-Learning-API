392a75008433422c0a3478f9049600fa503fb7a1
kapcodeO <kapilojha953@gmail.com>
2025-07-13 15:40:47 +0530

    added models.py

--- models.py
+++ models.py
@@ -1,1 +1,16 @@
-from pee
+from peewee import SqLiteDatabase, TextField, CharField,  Model
+
+db = SqLiteDatabase(["translations.db"])
+
+class TranslationModel(Model):
+    text = TextField()
+    base_lang_code = CharField()
+    final_lang_model = CharField()
+    translation = TextField(null=True)
+
+    class Meta:
+        database = db
+
+db.connect()
+db.create_tables([TranslationModel])
+print("✅ Database Initialized")
