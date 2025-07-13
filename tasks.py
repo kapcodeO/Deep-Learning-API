7d4e08540c6361dd5c8a724966ec37468ad39677
kapcodeO <kapilojha953@gmail.com>
2025-07-13 22:32:02 +0530

    updated tasks.py

--- tasks.py
+++ tasks.py
@@ -3,7 +3,7 @@ from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
 # initialize model and tokenizer
 model_name = "Helsinki-NLP/opus-mt-en-hi"
-tokenizer = AutoTokenizer.from_pretrained(model_name, max_model_len=512)
+tokenizer = AutoTokenizer.from_pretrained(model_name, model_max_length=512)
 translator = AutoModelForSeq2SeqLM.from_pretrained(model_name)
 
 # task 1: store_translation()
@@ -17,7 +17,7 @@ def store_translation(t):
 # task2: run_translation()
 def run_translation(task_id: int):
     model = TranslationModel.get_by_id(task_id)
-    tokens = tokenizer(model.text, return_tensor="pt").input_ids
+    tokens = tokenizer(model.text, return_tensors="pt").input_ids
     outputs = translator.generate(tokens, max_new_tokens=512)
     translation = tokenizer.decode(outputs[0], skip_special_tokens=True)
     model.translation = translation
